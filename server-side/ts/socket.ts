e423b1873b853e6dda5ed5bd72d1d1929f36abe2
simarbagga401 <simarbagga401@pop-os.localdomain>
2021-05-12 18:22:16 +0530

    added a comment

--- server-side/ts/socket.ts
+++ server-side/ts/socket.ts
@@ -2,6 +2,7 @@ import { Socket } from 'socket.io';
 import { SocketStyleRequest } from './interfaces/index.interfaces';
 import { changeStyle } from './writeStyle';
 
+// test
 export const handleSocket = (connection: Socket): void => {
   connection.emit('serverConnected', {
     _id: connection.id,
